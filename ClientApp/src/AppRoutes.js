9d79fff44456a8550db809cded501e1db36664cc
Leshan <leshan0627@gmail.com>
2022-11-12 22:07:40 -0500

    feat: Add Signup.js

--- ClientApp/src/AppRoutes.js
+++ ClientApp/src/AppRoutes.js
@@ -1,20 +1,21 @@
 import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
+import { Signin } from "./components/Signin";
 
 const AppRoutes = [
   {
     index: true,
-    element: <Home />
+    element: <Signin />,
   },
   {
-    path: '/counter',
-    element: <Counter />
+    path: "/counter",
+    element: <Counter />,
   },
   {
-    path: '/fetch-data',
-    element: <FetchData />
-  }
+    path: "/fetch-data",
+    element: <FetchData />,
+  },
 ];
 
 export default AppRoutes;
