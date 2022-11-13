bf4a1969a2f009e55d6d0ddbb28509c090f74475
Leshan <leshan0627@gmail.com>
2022-11-13 02:58:30 -0500

    docs: Update Sidebare.js

---
+++ Models/UserLogin.cs
@@ -0,0 +1,9 @@
+namespace app.dial
+
+{
+    public class User {
+        public int Id {get; set;}
+        public string UserName {get; set;}
+        public string UserPassword {get; set;}
+    }
+}
